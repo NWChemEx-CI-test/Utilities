6d401a8a072f7ddaf5081d9986e3ca40f9f01f48
yzhang <yzhang23@iastate.edu>
2023-08-18 15:45:29 -0500

    test container action with argument

--- actions/image-action/docker-action/build.sh
+++ actions/image-action/docker-action/build.sh
@@ -5,6 +5,6 @@ gcc_version="${gcc_version##* }"
 
 echo "In the base image container"
 cat /etc/issue
-echo "env1 = " $1
-echo "env2 = " $2
+echo "env1 = " ${e_env1}
+echo "env2 = " ${e_env2}
 echo "::set-output name=gcc_version::${gcc_version}"
