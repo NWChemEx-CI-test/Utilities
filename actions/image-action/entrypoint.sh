ed96f5b561e834c2419e81cd246dada33789860e
yzhang <yzhang23@iastate.edu>
2023-08-18 14:40:04 -0500

    test container action with argument

--- actions/image-action/entrypoint.sh
+++ actions/image-action/entrypoint.sh
@@ -6,9 +6,12 @@ BASE_TAG=$3
 ENV1=$4
 ENV2=$5
 
+echo "In the outer container:"
+echo $ENV1, $ENV2
+
 cd /docker-action
 
 #export CR_PAT=YOUR_TOKEN
 echo $CR_PAT | docker login ghcr.io -u $USER --password-stdin
 
-docker build -t docker-action --build-arg btag="$BASE_TAG" --build-arg env1=$ENV1 --build-arg env2=$ENV2 . && docker run docker-action 
+docker build -t docker-action --build-arg btag="$BASE_TAG" --build-arg env1="$ENV1" --build-arg env2="$ENV2" . && docker run docker-action 
