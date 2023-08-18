471f4e0a6b45f8085f4faa3c5fdd906d140e72b9
yzhang <yzhang23@iastate.edu>
2023-08-18 10:57:42 -0500

    test container action with argument

--- actions/image-action/entrypoint.sh
+++ actions/image-action/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -l
 
-BASE_TAG=$1
+CR_PAT=$1
+USER=$2
+BASE_TAG=$3
 
 cd /docker-action
+
+#export CR_PAT=YOUR_TOKEN
+echo $CR_PAT | docker login ghcr.io -u $USER --password-stdin
+
 docker build -t docker-action --build-arg btag="$BASE_TAG" . && docker run docker-action
