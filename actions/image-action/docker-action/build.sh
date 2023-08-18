f2b86ad73124850973ccc02b147e2e37e79cde36
yzhang <yzhang23@iastate.edu>
2023-08-18 14:02:13 -0500

    test container action with argument

--- actions/image-action/docker-action/build.sh
+++ actions/image-action/docker-action/build.sh
@@ -5,4 +5,6 @@ gcc_version="${gcc_version##* }"
 
 echo "In the base image container"
 cat /etc/issue
+echo "env1 = " $env1
+echo "env2 = " $env2
 echo "::set-output name=gcc_version::${gcc_version}"
--- actions/image-action/entrypoint.sh
+++ actions/image-action/entrypoint.sh
@@ -3,10 +3,12 @@
 CR_PAT=$1
 USER=$2
 BASE_TAG=$3
+ENV1=$4
+ENV2=$5
 
 cd /docker-action
 
 #export CR_PAT=YOUR_TOKEN
 echo $CR_PAT | docker login ghcr.io -u $USER --password-stdin
 
-docker build -t docker-action --build-arg btag="$BASE_TAG" . && docker run docker-action 
+docker build -t docker-action --build-arg btag="$BASE_TAG" . && docker run docker-action -e env1=$ENV1 -e env2=ENV2
