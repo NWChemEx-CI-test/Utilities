676d1c893dcb690815fa6e08c354093a9c028a3a
yzhang <yzhang23@iastate.edu>
2023-08-18 16:57:55 -0500

    test container action with argument

--- actions/image-action/docker-action/build.sh
+++ actions/image-action/docker-action/build.sh
@@ -7,6 +7,5 @@ echo "In the base image container"
 cat /etc/issue
 echo "env1 = " ${e_env1}
 echo "env2 = " ${e_env2}
-#echo "::set-output name=gcc_version::${gcc_version}"
-#echo "gcc_version=$gcc_version">>$env:GITHUB_OUTPUT
-echo "gcc_version=$gcc_version"
+echo "::set-output name=gcc_version::${gcc_version}"
+#echo "gcc_version=$gcc_version">>$GITHUB_OUTPUT
