b21f1c46634ab9031f12fb99789a82e0a5d4af9e
yzhang <yzhang23@iastate.edu>
2023-08-18 16:39:54 -0500

    test container action with argument

--- actions/image-action/docker-action/build.sh
+++ actions/image-action/docker-action/build.sh
@@ -8,3 +8,4 @@ cat /etc/issue
 echo "env1 = " ${e_env1}
 echo "env2 = " ${e_env2}
 #echo "::set-output name=gcc_version::${gcc_version}"
+echo "gcc_version=$gcc_version">>$env:GITHUB_OUTPUT
--- actions/image-action/entrypoint.sh
+++ actions/image-action/entrypoint.sh
@@ -7,11 +7,9 @@ ENV1=$4
 ENV2=$5
 
 echo "In the outer container:"
-echo $ENV1, $ENV2
 
 cd /docker-action
 
-#export CR_PAT=YOUR_TOKEN
 echo $CR_PAT | docker login ghcr.io -u $USER --password-stdin
 
-docker build -t docker-action --build-arg btag="$BASE_TAG" --build-arg env1="$ENV1" --build-arg env2="$ENV2" . && docker run docker-action echo "gcc_version=${gcc_version}">>$GITHUB_OUTPUT
+docker build -t docker-action --build-arg btag=$BASE_TAG --build-arg env1=$ENV1 --build-arg env2=$ENV2 . && docker run docker-action 
